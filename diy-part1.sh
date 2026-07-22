aff1cd19d4a05fa21544d3122816ba19b8368242
3121500 <77520355+3121500@users.noreply.github.com>
2026-07-22 11:56:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# 添加 PassWall2 软件源
 echo "Add PassWall2 feeds"
+
+cd openwrt
 
 grep -qxF "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git" feeds.conf.default || \
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git" >> feeds.conf.default
