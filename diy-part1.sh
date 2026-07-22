086049ff50532c263f2a6d9390e38b7aed37f379
3121500 <77520355+3121500@users.noreply.github.com>
2026-07-22 12:19:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,8 +4,7 @@ echo "Add PassWall2 feeds"
 
 cd openwrt
 
-grep -qxF "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git" feeds.conf.default || \
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git" >> feeds.conf.default
+sed -i '/passwall/d' feeds.conf.default
 
-grep -qxF "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git" feeds.conf.default || \
-echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git" >> feeds.conf.default
+echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git" >> feeds.conf.default
+echo "src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git" >> feeds.conf.default
