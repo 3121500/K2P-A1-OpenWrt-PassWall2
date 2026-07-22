8268b31ced28a1f0bcedc36898a941e99a919527
3121500 <77520355+3121500@users.noreply.github.com>
2026-07-22 13:46:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# 默认语言
-sed -i 's/option lang .*/option lang zh_cn/' package/base-files/files/etc/config/luci
+cd openwrt
 
-# 默认时区
-sed -i 's/option timezone .*/option timezone CST-8/' package/base-files/files/etc/config/system
+echo "Add PassWall2 feeds"
 
-# 开启软件流量分载
-mkdir -p files/etc/config
+sed -i '/passwall/d' feeds.conf.default
 
-cat > files/etc/config/firewall <<EOF
-config defaults
-        option flow_offloading '1'
-        option flow_offloading_hw '1'
-EOF
+echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git" >> feeds.conf.default
+
+echo "src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git" >> feeds.conf.default
